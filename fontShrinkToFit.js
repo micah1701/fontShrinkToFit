a02c6b3c50a295a64d72404134e1c793395d5002
Micah J. Murray <micah.murray@gmail.com>
2012-10-18 11:56:06 -0300

    added callback() functionality
    Custom function can be passed to plugin that is called when and element is done being shrunk.
    Also cleaned up some of the code.

--- fontShrinkToFit.js
+++ fontShrinkToFit.js
@@ -16,14 +16,15 @@
 $.fn.fontShrinkToFit = function(settings) {
 
 	var settings = $.extend( {
-		'maxHeight'		: null,	// eg "400" pixels.  if using this setting, be sure to set 'maxLines' to null or a value higher than 1.
-		'maxLines' 		: 1,	// maximum number of lines to wrap text on to.  set to null to ignore this directive
-		'fontSizeType'	: 'px', // 'px', 'em', 'pt'
-		'lineHeight'	: 1.2, 	// calculate line height between multiple lines. This is a percentage of the new font size. default is 120%
-		'shrinkBy'		: 1, 	// units to incrementally reduce font size by.  eg "1" pixel or ".1" em
+		'maxHeight'	: null,	// eg "400" pixels.  if using this setting, be sure to set 'maxLines' to null or a value higher than 1.
+		'maxLines'	: 1,	// maximum number of lines to wrap text on to.  set to null to ignore this directive
+		'fontSizeType'	: 'px',	// 'px', 'em', 'pt'
+		'lineHeight'	: 1.2,	// calculate line height between multiple lines. This is a percentage of the new font size. default is 120%
+		'shrinkBy'	: 1,	// units to incrementally reduce font size by.  eg "1" pixel or ".1" em
 		'minFontSize'	: 10,	// eg "10" pixels or "0.5" em -- the function will stop reducing the font once it gets to this size
-		'maxAttempts'	: 50	// number of incremental attempts to reduce the size before stopping
-    }, settings);
+		'maxAttempts'	: 50,	// number of incremental attempts to reduce the size before stopping
+		'callback'	: function(){ return true; }  // custom function that triggers when fontShrinkToFit() is finished
+    	}, settings);
 
     return this.each(function() {  
 		var block = $(this),
@@ -50,15 +51,15 @@ $.fn.fontShrinkToFit = function(settings) {
 			  	)
 			  )
 			{
-				//var newfontsize = parseInt(currentFont - settings['shrinkBy']) + settings['fontSizeType'];
-				//block.css("font-size",newfontsize);
-				
 				var newfontsize = parseInt(currentFont - settings['shrinkBy']); 
 				var newlineheight = (newfontsize * settings['lineHeight']) + settings['fontSizeType'];
 				newfontsize += settings['fontSizeType'];
 				block.css({"font-size":newfontsize,"line-height":newlineheight});
 				
 				f(attempt+1);
+			}else{
+				var callback = settings['callback'];
+				callback(block);
 			}
 		};
 		f(0);  
