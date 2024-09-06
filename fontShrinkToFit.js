728169ddca9dce93afac3080125f4f4b02a3877d
Micah J. Murray <micah.murray@gmail.com>
2024-09-06 17:24:15 -0400

    converts function to vanilla JS
    jQuery has been removed and there are breaking changes from version 1.2

--- fontShrinkToFit.js
+++ fontShrinkToFit.js
@@ -1,89 +1,92 @@
 /** 
  * fontShrinkToFit
  *
- * 2013 Micah J. Murray
- * micahj.com
+ * 2013-2024 Micah J. Murray
+ * creativeAdhocSolutions.com
  *
  * https://github.com/micah1701/fontShrinkToFit/
- * v1.2
+ * v2.0.0
  *
  * Usage:
  * set css font-size to max desired size for ideal amount of text in defined area 
- * then call like this:
- * 		$("#element").fontShrinkToFit({ optional:'settings' });
+ * then:
+ * const elements = Array.from(document.querySelectorAll('.your-selector'));
+ * fontShrinkToFit(elements, {
+ *     maxHeight: 400,
+ *     maxLines: 2,
+ *     fontSizeType: 'px',
+ *     lineHeight: 1.2,
+ *     shrinkBy: 1,
+ *     minFontSize: 10,
+ *     maxAttempts: 50,
+ *     allToSmallest: false,
+ *     callback: (element) => { console.log(`Shrunk: ${element}`); },
+ *     finished: () => { console.log("Finished shrinking all elements"); }
+ * }); 
  *
  */
-$.fn.fontShrinkToFit = function(settings) {
 
-	var settings = $.extend( {
-		'maxHeight'	: null,	 // eg "400" pixels.  if using this setting, be sure to set 'maxLines' to null or a value higher than 1.
-		'maxLines'	: 1,	 // maximum number of lines to wrap text on to.  set to null to ignore this directive
-		'fontSizeType'	: 'px',	 // 'px', 'em', 'pt'
-		'lineHeight'	: 1.2,	 // calculate line height between multiple lines. This is a percentage of the new font size. default is 120%
-		'shrinkBy'	: 1,	 // units to incrementally reduce font size by.  eg "1" pixel or ".1" em
-		'minFontSize'	: 10,	 // eg "10" pixels or "0.5" em -- the function will stop reducing the font once it gets to this size
-		'maxAttempts'	: 50,	 // number of incremental attempts to reduce the size before stopping
-		'allToSmallest'	: false,  // if being applied to multiple elements, shrink all elements to match the smallest one
-		'callback'	: function(){ return true; }, // custom function that triggers when fontShrinkToFit() is finished with each element
-		'finished'	: function(){ return true; }  // custom function that triggers when fontShrinkToFit() has finished running
-    }, settings);
+function fontShrinkToFit(elements, options = {}) {
+    const settings = Object.assign({
+        maxHeight: null,  // eg "400" pixels. If using this setting, be sure to set 'maxLines' to null or a value higher than 1.
+        maxLines: 1,  // Maximum number of lines to wrap text on to. Set to null to ignore this directive
+        fontSizeType: 'px',  // 'px', 'em', 'pt'
+        lineHeight: 1.2,  // Calculate line height between multiple lines. This is a percentage of the new font size. Default is 120%
+        shrinkBy: 1,  // Units to incrementally reduce font size by. eg "1" pixel or ".1" em
+        minFontSize: 10,  // eg "10" pixels or "0.5" em. The function will stop reducing the font once it gets to this size
+        maxAttempts: 50,  // Number of incremental attempts to reduce the size before stopping
+        allToSmallest: false,  // If being applied to multiple elements, shrink all elements to match the smallest one
+        callback: () => true,  // Custom function that triggers when fontShrinkToFit() is finished with each element
+        finished: () => true  // Custom function that triggers when fontShrinkToFit() has finished running
+    }, options);
 
-    var smallestFont = 99999,
-		shrink = this.each(function() {  
-		var block = $(this),
-			blockHeight = false,
-			f = function(attempt){		
-				var blockText = block.html(),
-					currentFont = parseInt( block.css("font-size")),
-					currentHeight = block.height();
-				
-				if(settings['maxLines'] != null && blockHeight === false){ 
-					blockHeight = block.html("X").height(); // calculate the height of a single line
-					block.html(blockText);
-				}
-				
-				if( (
-					  ( settings['maxLines']  != null && currentHeight > parseInt( blockHeight * settings['maxLines'] ) ) ||
-					  ( settings['maxHeight'] != null && currentHeight > settings['maxHeight'] ) 
-					)	
-					&&
-					(		
-					  settings['minFontSize'] != null && currentFont > settings['minFontSize'] &&
-					  attempt < settings['maxAttempts'] 
-					)
-				  )
-				{
-					var newfontsize = parseInt(currentFont - settings['shrinkBy']); 
-					var newlineheight = Math.floor(newfontsize * settings['lineHeight']) + settings['fontSizeType'];
-					newfontsize += settings['fontSizeType'];
-					block.css({"font-size":newfontsize,"line-height":newlineheight});
-					
-					f(attempt+1);
-				
-				}else{
-					
-					if( currentFont < smallestFont)
-					{
-						smallestFont = currentFont;
-					}
-					
-					var callback = settings['callback'];
-					callback(block);
-				}				
-			};
-		f(0); 				 
-	});
-	
-	if(shrink)
-	{
-		if(settings['allToSmallest'])
-		{
-			this.each(function() {
-				$(this).css({"font-size":smallestFont});		
-			});
-		}
-		
-		var finished = settings['finished'];
-		finished();
-	}
-};
+    let smallestFont = 99999;
+
+    elements.forEach(el => {
+        let block = el;
+        let blockHeight = false;
+
+        const shrinkText = (attempt) => {
+            let blockText = block.innerHTML;
+            let currentFont = parseInt(window.getComputedStyle(block).fontSize);
+            let currentHeight = block.offsetHeight;
+
+            if (settings.maxLines !== null && blockHeight === false) {
+                block.innerHTML = "X";  // Calculate the height of a single line
+                blockHeight = block.offsetHeight;
+                block.innerHTML = blockText;
+            }
+
+            if (
+                (
+                    (settings.maxLines !== null && currentHeight > parseInt(blockHeight * settings.maxLines)) ||
+                    (settings.maxHeight !== null && currentHeight > settings.maxHeight)
+                ) &&
+                (settings.minFontSize !== null && currentFont > settings.minFontSize && attempt < settings.maxAttempts)
+            ) {
+                let newFontSize = currentFont - settings.shrinkBy;
+                let newLineHeight = Math.floor(newFontSize * settings.lineHeight) + settings.fontSizeType;
+                newFontSize += settings.fontSizeType;
+
+                block.style.fontSize = newFontSize;
+                block.style.lineHeight = newLineHeight;
+
+                shrinkText(attempt + 1);
+            } else {
+                if (currentFont < smallestFont) {
+                    smallestFont = currentFont;
+                }
+                settings.callback(block);
+            }
+        };
+        shrinkText(0);
+    });
+
+    if (settings.allToSmallest) {
+        elements.forEach(el => {
+            el.style.fontSize = smallestFont + settings.fontSizeType;
+        });
+    }
+
+    settings.finished();
+}
